485da835b621fdc067b5ae7d2149476720fdb511
loganjacksonwsu <60114363+loganjacksonwsu@users.noreply.github.com>
2020-04-15 17:11:13 -0400

    build.xml and unit test upload
    Files Uploaded
    - build.xml
    - build.properties
    - MemorizeNamesTest.java
    - OptionsTest.java

---
+++ Code/MemorizeNamesTest.java
@@ -0,0 +1,44 @@
+package me.logan.senpagamemory;
+
+import org.junit.Test;
+
+public class MemorizeNamesTest {
+    @Test
+    public void startGame() throws Exception {
+    }
+
+    @Test
+    public void selectNameType() throws Exception {
+    }
+
+    @Test
+    public void loadNamesFromFile() throws Exception {
+    }
+
+    @Test
+    public void populateNames() throws Exception {
+    }
+
+    @Test
+    public void writeToFile() throws Exception {
+    }
+
+    @Test
+    public void printNames() throws Exception {
+       /* MemorizeNames memorizeNamesGame = new MemorizeNames();
+        memorizeNamesGame.names.add("Test Name");
+        memorizeNamesGame.names.add("Test Name 2");
+        memorizeNamesGame.printNames();
+        */
+
+    }
+
+    @Test
+    public void guessNames() throws Exception {
+    }
+
+    @Test
+    public void calculateNumberCorrect() throws Exception {
+    }
+
+}
